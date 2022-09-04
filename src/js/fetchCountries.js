50153a37e7c93cb0cd0264f255173fb938563119
Igor Smorodsky <ingerard@gmail.com>
2022-09-04 20:20:26 +0300

    added async debounce

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,11 +1,34 @@
+import { debounce } from "lodash";
+
 const URL = "https://restcountries.com/v3.1/name/";
-
 const fields = "name,capital,population,flags,languages";
+const DEBOUNCE_DELAY = 300;
 
-export const getCountriesByName = (name) => {
-    return fetch(`${URL}${name}?fields=${fields}`, {
-        headers: {
-            Accept: "application/json",
-        },
-    });
+// Fetch countries by name
+export const fetchCountries = (name) => {
+        const fetched = () => {
+                return fetch(`${URL}${name}?fields=${fields}`, {
+                        headers: {
+                                Accept: "application/json",
+                        },
+                });
+        };
+
+        const debouncedFetch = asyncDebounce(fetched, DEBOUNCE_DELAY);
+
+        return debouncedFetch();
 };
+
+// Async debounce
+function asyncDebounce(func, wait) {
+        const debounced = debounce((resolve, reject, args) => {
+                func(...args)
+                        .then(resolve)
+                        .catch(reject);
+        }, wait);
+
+        return (...args) =>
+                new Promise((resolve, reject) => {
+                        debounced(resolve, reject, args);
+                });
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,4 @@
-import { getCountriesByName } from "./fetchCountries";
-import { debounce } from "lodash";
+import { fetchCountries } from "./fetchCountries";
 import "../css/styles.css";
 import Notiflix from "notiflix";
 
@@ -10,148 +9,155 @@ const countryInfoElem = document.querySelector(".country-info");
 const buttonsElem = document.querySelectorAll("button");
 let countriesRef;
 
-const DEBOUNCE_DELAY = 300;
 const NUMBERS_COUNTRIES_TOSHOW = 10;
 
 // Event on buttons typing
 buttonsElem.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-        // console.log(e.currentTarget);
+        btn.addEventListener("click", (e) => {
+                // console.log(e.currentTarget);
 
-        if (e.currentTarget.classList.contains("button-clear")) {
-            input.value = "";
-            inputHighlightElem.innerText = "";
-            clearData();
-        }
-        if (e.currentTarget.classList.contains("button-search")) {
+                if (e.currentTarget.classList.contains("button-clear")) {
+                        input.value = "";
+                        inputHighlightElem.innerText = "";
+                        clearData();
+                }
 
-            if (input.value == '')
-            Notiflix.Notify.info("Type the name of country for search");
+                if (e.currentTarget.classList.contains("button-search")) {
+                        if (input.value == "")
+                                Notiflix.Notify.info("Type the name of country for search");
 
-            debouncedGetCountries(input.value);
-        }
-    });
+                        getCountriesByName(input.value);
+                }
+        });
 });
 
 // Event on input typing
 input.addEventListener("input", () => {
-    inputHighlightElem.innerText = input.value;
-    debouncedGetCountries(input.value);
+        inputHighlightElem.innerText = input.value;
+
+        if (input.value == "") {
+                clearData();
+                return;
+        }
+
+        getCountriesByName(input.value);
 });
 
 // Clear all elements
 function clearData() {
-    countryInfoElem.innerHTML = "";
-    countryListElem.innerHTML = "";
+        countryInfoElem.innerHTML = "";
+        countryListElem.innerHTML = "";
 }
 
-// Debounce func for get req
-const debouncedGetCountries = debounce((inputValue) => {
+// Post http req and get names
+function getCountriesByName(inputValue) {
+        fetchCountries(inputValue)
+                .then((response) => {
+                        if (!response.ok) {
+                                throw new Error(response.status);
+                        }
 
-    if (inputValue == "") {
-        clearData();
-        return;
-    }
+                        return response.json();
+                })
 
-    // Post http req and get names
-    getCountriesByName(inputValue)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json();
-        })
-        .then((foundedCountries) => {
-            try {
-                showCountries(foundedCountries);
-            } catch (error) {
-                console.log(error);
-                Notiflix.Notify.failure(error.message);
-            }
-        })
-        .catch(() => {
-            Notiflix.Notify.failure("Oops, there is no country with that name");
-        });
+                .then((foundedCountries) => {
+                        try {
+                                showCountries(foundedCountries);
+                        } catch (error) {
+                                console.log(error);
 
-}, DEBOUNCE_DELAY);
+                                Notiflix.Notify.failure(error.message);
+                        }
+                })
+
+                .catch(() => {
+                        Notiflix.Notify.failure("Oops, there is no country with that name");
+                });
+}
 
 // Show names of countries
 function showCountries(countries) {
-    if (countries != undefined) {
-        if (countries.length > NUMBERS_COUNTRIES_TOSHOW) {
-            Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
-            return;
+        if (countries != undefined) {
+                if (countries.length > NUMBERS_COUNTRIES_TOSHOW) {
+                        Notiflix.Notify.info(
+                                "Too many matches found. Please enter a more specific name.",
+                        );
+
+                        return;
+                }
+
+                if (countries.length == 1) {
+                        renderInfo(countries[0]);
+
+                        return;
+                }
+
+                if (countries.length <= NUMBERS_COUNTRIES_TOSHOW && countries.length > 1) {
+                        renderList(countries);
+
+                        return;
+                }
         }
 
-        if (countries.length == 1) {
-            renderInfo(countries[0]);
-            return;
-        }
-
-        if (countries.length <= NUMBERS_COUNTRIES_TOSHOW && countries.length > 1) {
-            renderList(countries);
-            return;
-        }
-    }
-
-    throw new Error("Oops, data is not correct");
+        throw new Error("Oops, data is not correct");
 }
 
 // Rendering list in html
 function renderList(countries) {
-    const markup = countries
-        .map((country, id) => {
-            const {
-                name: { official },
-                flags: { svg },
-            } = country;
+        const markup = countries
 
-            return `
+                .map((country, id) => {
+                        const {
+                                name: { official },
+                                flags: { svg },
+                        } = country;
+
+                        return `
                     <li class="country-item" data-id=${id}>
                         <img class="country-pic" src="${svg}" alt="${official} flag" />
                         <p>${official}</p>
                     </li>
                     `;
-        })
-        .join("");
+                })
+                .join("");
 
-    clearData();
-    countryListElem.innerHTML = markup;
+        clearData();
+        countryListElem.innerHTML = markup;
 
-    // Start listening click event on Items
-    countriesRef = countries;
-    setEventOnItems(countryListElem, countries);
+        // Start listening click event on Items
+        countriesRef = countries;
+        setEventOnItems(countryListElem, countries);
 }
 
 // Set event on item click
 function setEventOnItems(countryListElem) {
-    countryListElem.addEventListener("click", renderDetailOnClick);
+        countryListElem.addEventListener("click", renderDetailOnClick);
 }
 
 // Render info for a country in the list, that is clicked
 function renderDetailOnClick(e) {
-    const elem = e.target.parentNode;
-    if (elem.nodeName == "LI") {
-        // Get id from target elem
-        const id = elem.getAttribute("data-id");
-        // Show detail of selected country
-        renderInfo(countriesRef[id]);
-        // Remove event
-        countryListElem.removeEventListener("click", renderDetailOnClick, false);
-    }
+        const elem = e.target.parentNode;
+        if (elem.nodeName == "LI") {
+                // Get id from target elem
+                const id = elem.getAttribute("data-id");
+                // Show detail of selected country
+                renderInfo(countriesRef[id]);
+                // Remove event
+                countryListElem.removeEventListener("click", renderDetailOnClick, false);
+        }
 }
 
 // Rendering detail info for country
 function renderInfo(country) {
-    const {
-        name: { official },
-        flags: { svg },
-        capital,
-        population,
-        languages,
-    } = country;
+        const {
+                name: { official },
+                flags: { svg },
+                capital,
+                population,
+                languages,
+        } = country;
 
-    const markup = `
+        const markup = `
             <ul class="country-info-list">
                 <li class="country-info-item large">
                     <img class="country-pic" src="${svg}" alt="${official} flag" />
@@ -165,11 +171,11 @@ function renderInfo(country) {
                 </li>
                 <li class="country-info-item">
                     <p><span class="country-label">Languages: </span> ${Object.values(
-                        languages,
+                            languages,
                     ).join(", ")}</p>
                 </li>
             </ul>
     `;
-    clearData();
-    countryInfoElem.innerHTML = markup;
+        clearData();
+        countryInfoElem.innerHTML = markup;
 }
